e7900dfc925ab5c34efb1b9c543ed3eb891bb246
Kevin SALANIER <kevin.salanier@epitech.eu>
2025-03-17 14:23:42 +0100

    Add main.c file with initial Hello EPITECH program

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** epitech-dop-bschocolatine
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello EPITECH\n");
+    printf("This is a debug file\n");
+    return 0;
+}
